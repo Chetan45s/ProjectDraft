2ce7b2f9e946c48024adc62d7ab0c73076afa37f
Chetan45s <chetansalmotra45@gmail.com>
2022-03-07 19:48:41 +0530

    Add

--- GetDraft/settings.py
+++ GetDraft/settings.py
@@ -134,9 +134,11 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
